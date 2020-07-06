156cf1e2ef8843e88b8214d1c2e41aeecbb8c782
ArturToJa <paszynski.art@gmail.com>
2020-07-06 21:04:45 +0200

    Initial commit

---
+++ Source/BPTopDown/MyBlueprintFunctionLibrary.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyBlueprintFunctionLibrary.h"
+
+int UMyBlueprintFunctionLibrary::BitchesSumMega(int a, int b)
+{
+	return 2 * a + 2 * b;
+}
---
+++ Source/BPTopDown/MyTestActor.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyTestActor.h"
+
+// Sets default values
+AMyTestActor::AMyTestActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyTestActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyTestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+int AMyTestActor::MegaSumBitches(int a, int b)
+{
+	return 2 * a + 2 * b;
+}
+
